1d962b21820b5e8adde48029d3df707b21aa0f66
tsuguri <tsuguri4429@gmail.com>
2025-05-25 22:22:22 +0900

    feat: create GameViewModel

---
+++ unit4-navigation-and-app-architecture/pathway1-architecture-components/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/unscramble/ui/GameUiState.kt
@@ -0,0 +1,4 @@
+package com.example.unscramble.ui
+
+class GameUiState {
+}
---
+++ unit4-navigation-and-app-architecture/pathway1-architecture-components/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -0,0 +1,43 @@
+import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.allWords
+import com.example.unscramble.ui.GameUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class GameViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(GameUiState())
+    val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
+
+    init {
+        resetGame()
+    }
+
+    private lateinit var currentWord: String
+    private var usedWords: MutableSet<String> = mutableSetOf()
+
+    private fun pickRandomWordAndShuffle(): String {
+        currentWord = allWords.random()
+        if (usedWords.contains(currentWord)) {
+            return pickRandomWordAndShuffle()
+        } else {
+            usedWords.add(currentWord)
+            return shuffleCurrentWord(currentWord)
+        }
+    }
+
+    private fun shuffleCurrentWord(word: String): String {
+        val tempWord = word.toCharArray()
+        // Scramble the word
+        tempWord.shuffle()
+        while (String(tempWord).equals(word)) {
+            tempWord.shuffle()
+        }
+        return String(tempWord)
+    }
+
+    fun resetGame() {
+        usedWords.clear()
+        _uiState.value = GameUiState(currentScrambledWord = pickRandomWordAndShuffle())
+    }
+}
