86504cda423e9780752056654fdcd77991346115
tsuguri <tsuguri4429@gmail.com>
2025-05-26 21:51:37 +0900

    feat: show game over dialog

--- unit4-navigation-and-app-architecture/pathway1-architecture-components/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ unit4-navigation-and-app-architecture/pathway1-architecture-components/basic-android-kotlin-compose-training-unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -2,6 +2,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.unscramble.data.MAX_NO_OF_WORDS
 import com.example.unscramble.data.SCORE_INCREASE
 import com.example.unscramble.data.allWords
 import com.example.unscramble.ui.GameUiState
@@ -44,14 +45,25 @@ class GameViewModel : ViewModel() {
     }
 
     private fun updateGameState(updatedScore: Int) {
-        _uiState.update { currentState ->
-            currentState.copy(
-                isGuessedWordWrong = false,
-                currentScrambledWord = pickRandomWordAndShuffle(),
-                score = updatedScore,
-                currentWordCount = currentState.currentWordCount.inc(),
-            )
+        if (usedWords.size == MAX_NO_OF_WORDS) {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    score = updatedScore,
+                    isGameOver = true
+                )
+            }
+        } else {
+            _uiState.update { currentState ->
+                currentState.copy(
+                    isGuessedWordWrong = false,
+                    currentScrambledWord = pickRandomWordAndShuffle(),
+                    score = updatedScore,
+                    currentWordCount = currentState.currentWordCount.inc(),
+                )
+            }
         }
+
     }
 
     fun resetGame() {
