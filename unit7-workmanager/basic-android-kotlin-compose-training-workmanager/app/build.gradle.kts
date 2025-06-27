263d182044f27903d41200a3e6cf8f37a7c65160
tsuguri <tsuguri4429@gmail.com>
2025-06-27 17:24:24 +0900

    create starter project

--- unit7-workmanager/basic-android-kotlin-compose-training-workmanager/app/build.gradle.kts
+++ unit7-workmanager/basic-android-kotlin-compose-training-workmanager/app/build.gradle.kts
@@ -81,13 +81,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.work:work-runtime-ktx:2.10.0")
-    implementation("androidx.work:work-runtime-ktx:2.10.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
-
-    // Work testing
-    androidTestImplementation("androidx.work:work-testing:2.10.0")
-
 }
